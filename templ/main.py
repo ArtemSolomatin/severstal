5869542af9f666d8f29f1080961e3b555e9b1eba
ivan_korostelev <i.korostelev@cdnvideo.ru>
2019-02-23 19:52:05 +0300

    Orb+template

--- templ/main.py
+++ templ/main.py
@@ -5,10 +5,10 @@ from os import listdir
 from os.path import isfile, join
 
 # Directory with base images
-COLOR = 0 # 0 for gray, -1 for color
-IM_DIR = '/home/ivan23kor/Hackathon/templ/Severstal/'
-DET_DIR = IM_DIR + 'detected/'
-template = 'envelope.png'
+COLOR = -1 # 0 for gray, -1 for color
+IM_DIR = './our_dataset/'
+DET_DIR = join(IM_DIR, 'detected/')
+template = join(IM_DIR, 'phone.jpg')
 
 # Template image
 template = cv2.imread(template, COLOR)
@@ -16,22 +16,29 @@ template = cv2.imread(template, COLOR)
 # Comparison methods
 methods = [cv2.TM_SQDIFF_NORMED, cv2.TM_CCORR_NORMED, cv2.TM_CCOEFF_NORMED]
 
-
 # Every image
 for image_name in [f for f in listdir(IM_DIR) if isfile(join(IM_DIR, f))]:
+    if image_name[:-4] in ['cookie', 'phone', 'thing']:
+        continue
      # Open and show image
-    image = cv2.imread(IM_DIR + image_name, COLOR)
+    image = cv2.imread(join(IM_DIR, image_name), COLOR)
 
     # Find flower
-    for i, method in enumerate(methods[0:], 0):
+    for i, method in enumerate(methods[2:], 2):
+        # Apply filter
         result = cv2.matchTemplate(image, template, method)
 
-        threshold = 0.9
+        # Return best coincidences
+        threshold = 0.2
         loc = np.where(result >= threshold)
+
+        # Size retrieval depending on color
         if COLOR == 0:
             w, h = template.shape
         else:
             w, h = template.shape[:-1]
+
+        # Detected image
         detected = image.copy()
         for pt in zip(*loc[::-1]):
             cv2.rectangle(detected, pt, (pt[0] + h, pt[1] + w), (0, 255, 0), 1)
--- templ/orb.py
+++ templ/orb.py
@@ -4,44 +4,35 @@ import numpy as np
 from os import listdir
 from os.path import isfile, join
 
-# Directory with base images
-COLOR = 0 # 0 for gray, -1 for color
-IM_DIR = '/home/ivan23kor/Hackathon/templ/Severstal/'
-DET_DIR = IM_DIR + 'detected/'
-template = 'envelope.png'
+IM_DIR = './our_dataset/'
+template = IM_DIR + 'phone.jpg'
+COLOR = -1 # 0 for gray, -1 for color
+MATCH_DIR = IM_DIR + 'matched/'
 
 # Template image
 template = cv2.imread(template, COLOR)
 
-# Comparison methods
-methods = [cv2.TM_SQDIFF_NORMED, cv2.TM_CCORR_NORMED, cv2.TM_CCOEFF_NORMED]
-
-
-# Every image
+# Every image but templates
 for image_name in [f for f in listdir(IM_DIR) if isfile(join(IM_DIR, f))]:
+    if image_name[:-4] in ['cookie', 'phone', 'thing']:
+        continue
+
      # Open and show image
     image = cv2.imread(IM_DIR + image_name, COLOR)
 
-    # Find flower
-    for i, method in enumerate(methods[2:], 2):
-        # Initiate ORB detector
-        orb = cv2.ORB_create()
-        # find the keypoints and descriptors with ORB
-        kp1, des1 = orb.detectAndCompute(template,None)
-        kp2, des2 = orb.detectAndCompute(image,None)
+    # Initiate ORB detector
+    orb = cv2.ORB_create()
+    # find the keypoints and descriptors with ORB
+    kp1, des1 = orb.detectAndCompute(template, None)
+    kp2, des2 = orb.detectAndCompute(image, None)
 
-        # create BFMatcher object
-        bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
-        # Match descriptors.
-        matches = bf.match(des1, des2)
-        # Sort them in the order of their distance.
-        matches = sorted(matches, key = lambda x:x.distance)
-        print(matches)
-        # Draw first 10 matches.
-        img3 = image.copy()
-        img3 = cv2.drawMatches(template, kp1, image, kp2, matches[:10], img3, flags=2)
-        plt.imshow(img3),plt.show()
-
-        # # Show result
-        # detected_name = DET_DIR + image_name[:-4] + '_' + str(i) + image_name[-4:]
-        # cv2.imwrite(detected_name, detected)
+    # create BFMatcher object
+    bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
+    # Match descriptors.
+    matches = bf.match(des1, des2)
+    # Sort them in the order of their distance.
+    matches = sorted(matches, key=lambda x:x.distance)
+    # Draw first 10 matches.
+    match = image.copy()
+    match = cv2.drawMatches(template, kp1, image, kp2, matches[:5], match, flags=2)
+    cv2.imwrite(MATCH_DIR + image_name[:-4] + '_match.jpg', match)
