abfdec315675c75108ed47038fcb2642e8f45c63
ivan23kor <i.korostelev@cdnvideo.ru>
2019-02-24 04:19:28 +0300

    Fixed bug with ORB.\n Tuned ORB parameters.\n Now image opens fullscreen.

--- affine.py
+++ affine.py
@@ -5,23 +5,23 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-def orb(image1, image2, orb_size=500):
+def orb(image1, image2, nfeatures=500):
     # Detect ORB features and compute descriptors.
-    orb = cv2.ORB_create(orb_size)
+    orb = cv2.ORB_create(nfeatures=500, edgeThreshold=21, patchSize=21)
     keypoints1, descriptors1 = orb.detectAndCompute(image1, None)
     keypoints2, descriptors2 = orb.detectAndCompute(image2, None)
      
     # Match features.
-    matcher = cv2.DescriptorMatcher_create(cv2.DESCRIPTOR_MATCHER_BRUTEFORCE_HAMMING)
+    # matcher = cv2.DescriptorMatcher_create(cv2.DESCRIPTOR_MATCHER_BRUTEFORCE_HAMMING)
+    matcher = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
     matches = matcher.match(descriptors1, descriptors2, None)
      
     # Sort matches by score
     matches.sort(key=lambda x: x.distance, reverse=False)
  
     # Extract location of good matches
-    points1 = np.zeros((len(matches), 2), dtype=np.float32)
-    points2 = np.zeros((len(matches), 2), dtype=np.float32)
- 
+    points1 = np.empty((len(matches), 2), dtype=np.float32)
+    points2 = np.empty((len(matches), 2), dtype=np.float32)
     for i, match in enumerate(matches):
         points1[i, :] = keypoints1[match.queryIdx].pt
         points2[i, :] = keypoints2[match.trainIdx].pt
@@ -48,11 +48,9 @@ def rotate_images(image1, image2):
 
     return rotated_im, h
 
-def smart_subtract(image1, image2):
-    subtracted = cv2.subtract(image2, image1)
-    return subtracted
-
 def show_img(image, name='Image'):
+    cv2.namedWindow(name, cv2.WND_PROP_FULLSCREEN)
+    cv2.setWindowProperty(name, cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
     cv2.imshow(name, image)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
@@ -67,7 +65,13 @@ def generate_diff(img1, img2):
     return added
 
 if __name__ == '__main__':
-    img1 = cv2.imread('1.jpg')
-    img2 = cv2.imread('2.jpg')
+    img1 = cv2.imread('3.jpg')
+    img2 = cv2.imread('4.jpg')
     added = generate_diff(img1, img2)
-    show_img(added)
+
+    gray = cv2.cvtColor(added, cv2.COLOR_BGR2GRAY)
+    kernel = np.ones((1, 1), np.uint8)
+    dilated = cv2.dilate(gray, kernel, iterations=1) 
+    eroded = cv2.erode(dilated, kernel, iterations=1) 
+    blurred = cv2.GaussianBlur(eroded, (5, 5), 2)
+    show_img(eroded)
